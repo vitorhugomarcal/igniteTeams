d28081ed7da312d93ddb510f8b7cdcc390e047ca
vitorhugomarcal <vitorhugomarcal@gmail.com>
2022-10-24 20:23:32 -0300

    Add: font Roboto & Statusbar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups';
+import theme from './src/theme';
 import { StatusBar } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
-      <Groups />
-    </>
+      {fontsLoaded ? <Groups /> : <Loading /> }
+    </ThemeProvider>
   );
 }
